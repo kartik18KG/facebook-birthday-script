bc90deea35c3fb3eaebc0241e08bfe8d8a3c0d42
Kartik <kartikdps.kg@gmail.com>
2019-07-03 20:12:06 +0530

    first commit

---
+++ project.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+
+prefs = {
+    "profile.default_content_setting_values.notifications": 2,
+    "profile.default_content_settings.popups": 0,
+}
+chrome_options = webdriver.ChromeOptions()
+
+chrome_options.add_experimental_option("prefs", prefs)
+
+
+username = "9610526978"
+password = "travel@123"
+url = "https://www.facebook.com"
+driver = webdriver.Chrome("/Users/kartikgupta/Downloads/chromedriver")
+driver.get(url)
+driver.find_element_by_id("email").send_keys(username)
+driver.find_element_by_id("pass").send_keys(password)
+driver.find_element_by_id("loginbutton").click()
+driver.get("https://www.facebook.com/events/birthdays/")
+feed = "Happy Birthday Dude!"
+element = driver.find_elements_by_xpath(
+    "//*[@class ='enter_submit uiTextareaNoResize uiTextareaAutogrow uiStreamInlineTextarea inlineReplyTextArea mentionsTextarea textInput']"
+)
+
+for el in element:
+    cnt += 1
+    element_id = str(el.get_attribute("id"))
+    print(element_id)
+    XPATH = '//*[@id ="' + element_id + '"]'
+    post_field = driver.find_element_by_xpath(XPATH)
+    post_field.send_keys(feed)
+
